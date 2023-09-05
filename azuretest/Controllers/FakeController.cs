80f6a98bb22a96d40be473759535e7f2ce30bcd0
dicentes <61921412+dicentes@users.noreply.github.com>
2023-09-04 21:47:31 -0400

    second try

---
+++ azuretest/Controllers/FakeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace azuretest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FakeController : ControllerBase
+{
+    
+    private readonly ILogger<FakeController> _logger;
+
+    public FakeController(ILogger<FakeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    // make an API call to https://jsonplaceholder.typicode.com/todos/1
+    //put everything that it outputs, no matter what it outputs, into one string 
+    //return that string to the frontend 
+    
+}
