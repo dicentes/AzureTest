860a317d68b920816ec6028f082d1937a4476ce8
dicentes <61921412+dicentes@users.noreply.github.com>
2023-09-09 12:03:20 -0400

    image posted sortof improved

--- azuretest/ClientApp/src/components/Home.js
+++ azuretest/ClientApp/src/components/Home.js
@@ -1,22 +1,25 @@
+// Import section remains the same
 import React, { Component } from 'react';
 import discobolus from './noun-discobolus-1310560.svg';
 
 export class Home extends Component {
   static displayName = Home.name;
 
-
-  // Discobolus by Gonza from <a href="https://thenounproject.com/browse/icons/term/discobolus/" target="_blank" title="Discobolus Icons">Noun Project</a> (CC BY 3.0)
   render() {
     return (
-      <div className="d-flex justify-content-between">
-        <div>
+      // flex-grow-1 makes this div take available space but not more
+      <div className='content-wrapper'> 
+      <div className="d-flex justify-content-between flex-grow-1">  
+        <div className="textbox">
           <p>Welcome to Brocountability.</p>
           <p>Setting goals for yourself can be easy - completing them is hard. Brocountability is here to fix that.</p>
           <p>Register now to make an account and join other bros looking to improve themselves.</p>
         </div>
-        <img src={discobolus} alt="test icon" width="500" height="600" />
-        
+
+        {/* Set both width and height to 100% */}
+        <img src={discobolus} alt="test icon" className="img-fluid frontpageimage" />
       </div>
+      </div> 
     );
   }
 }
