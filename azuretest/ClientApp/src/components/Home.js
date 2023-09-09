56410bb070279e84a4a7a0b2bce2843dab328bfc
dicentes <61921412+dicentes@users.noreply.github.com>
2023-09-09 12:21:12 -0400

    butchered css again

--- azuretest/ClientApp/src/components/Home.js
+++ azuretest/ClientApp/src/components/Home.js
@@ -9,15 +9,15 @@ export class Home extends Component {
     return (
       // flex-grow-1 makes this div take available space but not more
       <div className='content-wrapper'> 
-      <div className="d-flex justify-content-between flex-grow-1">  
-        <div className="textbox">
+      <div className="d-flex bd-highlight ">  
+        <div className="textbox fs-1">
           <p>Welcome to Brocountability.</p>
-          <p>Setting goals for yourself can be easy - completing them is hard. Brocountability is here to fix that.</p>
+          <p>Setting goals for yourself can be easy - <i>completing </i>them is hard. Brocountability is here to fix that.</p>
           <p>Register now to make an account and join other bros looking to improve themselves.</p>
         </div>
 
         {/* Set both width and height to 100% */}
-        <img src={discobolus} alt="test icon" className="img-fluid frontpageimage" />
+        <img src={discobolus} alt="test icon" className="img-fluid frontpageimage flex-grow-1 " />
       </div>
       </div> 
     );
