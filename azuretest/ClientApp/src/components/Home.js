98bebb48712ea2098fbb113c1702a5c0fb82aca4
dicentes <61921412+dicentes@users.noreply.github.com>
2023-09-09 13:01:24 -0400

    IMPROVED CSS WORKING ON DESKTOP AND PHONE

--- azuretest/ClientApp/src/components/Home.js
+++ azuretest/ClientApp/src/components/Home.js
@@ -8,18 +8,19 @@ export class Home extends Component {
   render() {
     return (
       // flex-grow-1 makes this div take available space but not more
-      <div className='content-wrapper'> 
-      <div className="d-flex bd-highlight ">  
+       
+      <div className="d-flex bd-highlight justify-content-evenly">  
         <div className="textbox fs-1">
           <p>Welcome to Brocountability.</p>
-          <p>Setting goals for yourself can be easy - <i>completing </i>them is hard. Brocountability is here to fix that.</p>
+          <p>Setting goals for yourself is easy - <i>completing </i>them is hard. </p>
+          <p> Brocountability is here to fix that.</p>
           <p>Register now to make an account and join other bros looking to improve themselves.</p>
         </div>
 
         {/* Set both width and height to 100% */}
-        <img src={discobolus} alt="test icon" className="img-fluid frontpageimage flex-grow-1 " />
+        <img src={discobolus} alt="test icon" className="img-fluid frontpageimage" />
       </div>
-      </div> 
+       
     );
   }
 }
