23c87a13e982e56136a3d7eba43c6d826659d5c0
dicentes <61921412+dicentes@users.noreply.github.com>
2023-09-09 21:47:28 -0400

    footer updated

--- azuretest/ClientApp/src/components/Footer.js
+++ azuretest/ClientApp/src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   
   return (
     <div className="footer">
-      <p> Made by Rob </p>
+      <p> Made by <a href="https://github.com/dicentes">Dicentes </a> </p>
       
     </div>
   );
